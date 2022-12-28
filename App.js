0b33ada2709943215dcd745dffc5a74b42772e41
Asadhashmy <asad@cloudornate.com>
2022-12-28 13:04:36 +0500

    challenge 1 done

--- App.js
+++ App.js
@@ -9,10 +9,16 @@ const App = () => {
   };
   return (
     <View>
-      <CustomComponent />
+      {/* <CustomComponent />
       <Text style={styles.textStyle}>Hellow {myVar} from App.js</Text>
       {myElement}
-      <Text>HI, {getFullName("Syed", "Asad")}</Text>
+      <Text>HI, {getFullName("Syed", "Asad")}</Text> */}
+      {/* Challenge 1 (35) */}
+      <Text style={styles.text1}>Welcome To Thapa Technical Channel</Text>
+      <Text style={styles.text2}>
+        We Love React Native and I am a subscriber of Thapa Technical Channel
+      </Text>
+      <Text>Hi, My name is {myVar}</Text>
     </View>
   );
 };
@@ -20,6 +26,14 @@ const App = () => {
 const styles = StyleSheet.create({
   textStyle: {
     color: "orange",
+  },
+  text1: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "red",
+  },
+  text2: {
+    fontSize: 30,
   },
 });
 export default App;
