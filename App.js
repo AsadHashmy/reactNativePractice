514be12795fe23bafd9372a9bc38149ba1dc10b1
Asadhashmy <asad@cloudornate.com>
2022-12-28 17:30:55 +0500

    images in react native implemented

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Text, StyleSheet, View } from "react-native";
-import FlatListDemo from "./src/screens/FlatListDemo";
+import ImageFile from "./src/screens/ImageFile";
 
 const App = () => {
   return (
     <View>
-      <FlatListDemo />
+      <ImageFile />
     </View>
   );
 };
--- src/screens/ImageFile.js
+++ src/screens/ImageFile.js
@@ -1,12 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 
 const ImageFile = () => {
   return (
-    <View>
-      <Text>This is ImageFile</Text>
+    <View style={styles.listStyle}>
+      <Text style={styles.textStyle}>This is ImageFile</Text>
+      <Image
+        style={styles.imageStyle}
+        source={require("../../assets/test.jpg")}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  listStyle: {
+    height: 500,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textStyle: {
+    fontSize: 30,
+  },
+  imageStyle: {
+    height: 300,
+    width: 300,
+  },
+});
 
 export default ImageFile;
